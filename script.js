d7dec7e41ec19cdeb606f2d861c67a5d4f9f9f93
sparsh0116 <ss0699@srmist.edu.in>
2022-12-19 23:24:46 +0530

    js

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Welcome to Tic Tac Toe")
+let turn = "X";
+
+//Function to change Turn
+
+
+const changeTurn = ()=>{
+    return turn === "X" ? "0" : "X"
+}
+
+//Function to check the winner
+
+const checkWin = ()=> {
+
+}
+
+
+//Game Logic
+
+let boxes = document.getElementsByClassName("gamebox")
